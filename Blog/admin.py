73898c20a15764ded7a12213c9c94efb0441cdd4
Mohammad Reza <mohammadrezasolo69@gmail.com>
2021-10-11 21:25:35 +0330

    created ==> models & admin Article / view list_article /

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ArticleModel
 
-# Register your models here.
+class ArticleModelAdmin(admin.ModelAdmin):
+    list_display = ['title','author', 'publish', 'status']
+    search_fields = ["title", 'author']
+    list_filter = ['author']
+    list_editable = ['status']
+
+admin.site.register(ArticleModel, ArticleModelAdmin)
+
+
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,14 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import ArticleModel
+
 
 # Create your views here.
+
+
+class List_article(ListView):
+    template_name = 'blog/list_article.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return ArticleModel.objects.filter(status=True)
