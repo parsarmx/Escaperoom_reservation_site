94b0b3e7d5409becee58fd29fe6fa4d68a75122a
parsarmx <pramezani92@gmail.com>
2021-10-16 19:02:15 +0330

    not important things changed

--- Escapee2/settings.py
+++ Escapee2/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking',
+    'booking.apps.BookingConfig',
     'django_jalali',
 
     # app Blog
--- booking/models.py
+++ booking/models.py
@@ -34,6 +34,7 @@ class Player(models.Model):
     number_of_players = models.IntegerField()
     email = models.EmailField()
     phone = models.CharField(max_length=20)
+    game = models.ForeignKey(EscapeRoom, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.name}, {self.last_name}'
@@ -55,13 +56,6 @@ class ReserveDate(models.Model):
 
     def __str__(self):
         return f'{self.date}'
-
-
-
-
-
-
-
 
 
 class ReserveTime(models.Model):
