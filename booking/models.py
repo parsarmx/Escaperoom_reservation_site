1802e99ff4bebc59a2cc8736770e71574c92514a
parsarmx <pramezani92@gmail.com>
2021-10-17 10:20:59 +0330

    ReserveTime and ReserveDate model Changed!

--- booking/models.py
+++ booking/models.py
@@ -42,6 +42,7 @@ class Player(models.Model):
 
 class ReserveDate(models.Model):
     date = models.DateField()
+    name = models.ForeignKey(EscapeRoom, on_delete=models.CASCADE, null=True)
 
     def j_date(self):
         return date2jalali(self.date)
@@ -49,15 +50,24 @@ class ReserveDate(models.Model):
     def jd_create_datetime(self):
         return jd.fromgregorian(year=self.date.year, month=self.date.month, day=self.date.day)
 
-    activate = models.BooleanField(default=False)
-    reserved = models.BooleanField(default=False)
-    name = models.ForeignKey(EscapeRoom, on_delete=models.CASCADE, null=True)
-    player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, blank=True)
-
     def __str__(self):
         return f'{self.date}'
 
 
 class ReserveTime(models.Model):
-    time = models.TimeField()
+    TIME_CHOICES = (
+        (datetime.time(12, 00), '12:00'), (datetime.time(12, 15), '12:15'), (datetime.time(12, 30), '12:30'),
+        (datetime.time(12, 45), '12:45'), (datetime.time(13, 00), '13:00'), (datetime.time(13, 15), '13:15'),
+        (datetime.time(13, 30), '13:30'), (datetime.time(13, 45), '13:45'), (datetime.time(14, 00), '14:00'),
+        (datetime.time(14, 15), '14:15'), (datetime.time(14, 30), '13:30'), (datetime.time(14, 45), '14:45'),
+        (datetime.time(15, 00), '15:00'), (datetime.time(15, 15), '15:15'), (datetime.time(15, 45), '15:45'),
+        (datetime.time(16, 00), '16:00'), (datetime.time(16, 15), '16:15'), (datetime.time(16, 30), '16:45'),
+        (datetime.time(17, 00), '17:00'), (datetime.time(17, 15), '17:15'), (datetime.time(17, 30), '17:30'),
+
+    )
+
+    time = models.TimeField(choices=TIME_CHOICES)
     date = models.ForeignKey(ReserveDate, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, blank=True)
+    game = models.ForeignKey(EscapeRoom, on_delete=models.CASCADE, null=True, blank=True)
+    status = models.BooleanField(default=False)
