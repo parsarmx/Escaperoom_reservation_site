36500e359d243d2afa07374183f92e7ff77c80e4
parsarmx <pramezani92@gmail.com>
2021-10-24 12:15:32 +0330

    created antoher functon for sms panel sms text fields updated using date_convertor.py and time choice model updated

--- booking/date_convertor.py
+++ booking/date_convertor.py
@@ -18,27 +18,27 @@ def week_day_convert(day):
 
 # this method does the same thing for months
 def month_convertor(month):
-    if month == 'Farvardin' or 1:
+    if month == 1:
         return 'فروردین'
-    elif month == 'Ordibehesht' or 2:
+    elif month == 2:
         return 'اردیبهشت'
-    elif month == 'Khordad' or 3:
+    elif month == 3:
         return 'خرداد'
-    elif month == 'Tir' or 4:
+    elif month == 4:
         return 'تیر'
-    elif month == 'Mordad' or 5:
+    elif month == 5:
         return 'مرداد'
-    elif month == 'Shahrivar' or 6:
+    elif month == 6:
         return 'شهریور'
-    elif month == 'Mehr' or 7:
+    elif month == 7:
         return 'مهر'
-    elif month == 'Aban' or 8:
+    elif month == 8:
         return 'آبان'
-    elif month == 'Azar' or 9:
+    elif month == 9:
         return 'آذر'
-    elif month == 'Dey' or 10:
+    elif month == 10:
         return 'دی'
-    elif month == 'Bahman' or 11:
+    elif month == 11:
         return 'بهمن'
-    elif month == 'Esfand' or 12:
+    elif month == 12:
         return 'اسفند'
